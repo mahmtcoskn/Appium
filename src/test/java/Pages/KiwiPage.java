7dba058f709ca82be16555caca709e55aaa87d8c
mahmtcoskn <mhmt_coskun@icloud.com>
2023-04-24 04:41:46 +0300

    appium

---
+++ src/test/java/Pages/KiwiPage.java
@@ -0,0 +1,50 @@
+package Pages;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
+import java.time.Duration;
+
+public class KiwiPage {
+    public KiwiPage(){
+        PageFactory.initElements((WebDriver) Driver.getAndroidDriver(),this);
+    }
+
+    @FindBy(xpath = "//*[@text='Continue as a guest']")
+    public WebElement asAGuest;
+
+    @FindBy(xpath = "//*[@text='Return']")
+    public WebElement returnButton;
+
+    @FindBy(xpath ="//*[@text='One way']" )
+    public WebElement oneWay;
+
+    @FindBy(xpath = "//*[@text='From:']")
+    public WebElement kalkisButonu;
+
+    @FindBy(xpath = "//android.view.View[@content-desc=\"Clear All\"]")
+    public WebElement defaultUlkeSILME;
+
+    @FindBy(xpath = "//*[@text='İzmir, Turkey']")
+    public WebElement izmir;
+
+    @FindBy(xpath = "//*[@text='Choose']")
+    public WebElement choose;
+
+    public static void ucButtonTiklama(int baslangic,int bitis,int xCoordinat,int yCoordinat,int wait){
+        TouchAction action = new TouchAction<>(Driver.getAndroidDriver());
+        for (int i=baslangic; i<bitis; i++){
+            action.press(PointOption.point(xCoordinat,yCoordinat)).
+                    waitAction(WaitOptions.waitOptions(Duration.ofMillis(wait)))
+                    .release().
+                    perform();
+
+        }
+    }
+}
---
+++ src/test/java/tests/KiwiTest.java
@@ -0,0 +1,66 @@
+package tests;
+
+import Pages.KiwiPage;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.Driver;
+
+public class KiwiTest {
+
+    // uygulamanin yuklendigi dogrulanir
+    // uygulamanin basariyla acildigi dogrulanir
+    // misafir olarak devam et e tiklanir
+    // ardinda gelecek olan 3 adimada yesil butona basilarak devam edilir
+    // Trip type, one way olarak secilir
+    // kalkis ulkesi secenegine tiklanir ve default olan ulke kaldirilir
+    // kalkis yapilacak ulke/sehir girilir ve sec e tiklanir
+    // varis ulkesi secenegine tiklanir ve gidilecek ulke girilir
+    // gidis tarihi mayis ayinin 21 i olarak secilir ve set date e tiklanir
+    // search butonuna tiklanir
+    // en ucuz ve aktarmasiz filtrelemeleri yapilir
+    // gelen bilet fiyati kaydedilir ve kullanicin telefonuna sms olarak gonderilir
+
+    AndroidDriver<AndroidElement> driver = Driver.getAndroidDriver();
+
+    KiwiPage page=new KiwiPage();
+
+    @Test
+    public void kiwiTest(){
+
+        // uygulamanin yuklendigi dogrulanir
+        Assert.assertTrue(driver.isAppInstalled("com.skypicker.main"),"uygulama yuklenemedi");
+
+        // uygulamanin basariyla acildigi dogrulanir
+        String yaziTextActual= page.asAGuest.getText();
+        String yaziTextExpected="Continue as a guest";
+        Assert.assertEquals(yaziTextActual,yaziTextExpected,"uygulama basarili bir sekilde baslatilamadi");
+
+        // misafir olarak devam et e tiklanir
+        page.asAGuest.click();
+
+        // ardinda gelecek olan 3 adimada yesil butona basilarak devam edilir
+        page.ucButtonTiklama(0,3,538,2029,500);
+
+        // Trip type,one way olarak secilir
+        page.returnButton.click();
+        page.oneWay.click();
+
+        // kalkis ulkesi secenegine tiklanir ve default olan ulke kaldirilir
+        page.kalkisButonu.click();
+        page.defaultUlkeSILME.click();
+
+        // kalkis yapilacak ulke/sehir girilir ve sec e tiklanir
+        driver.getKeyboard().pressKey("izmir");
+        page.izmir.click();
+        page.choose.click();
+
+        // varis ulkesi secenegine tiklanir ve gidilecek ulke girilir
+        // gidis tarihi mayis ayinin 21 i olarak secilir ve set date e tiklanir
+        // search butonuna tiklanir
+        // en  ucuz ve aktarmasiz filtrelemeleri yapilir
+        // gelen bilet fiyati kaydedilir ve kullanicin telefonuna sms olarak gonderilir
+
+    }
+}
