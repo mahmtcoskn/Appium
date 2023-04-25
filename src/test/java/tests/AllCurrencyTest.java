021db8143c31e022fd336ede648c4329b765ad25
mahmtcoskn <mhmt_coskun@icloud.com>
2023-04-25 22:55:34 +0300

    appium

--- src/test/java/tests/AllCurrencyTest.java
+++ src/test/java/tests/AllCurrencyTest.java
@@ -2,8 +2,16 @@ package tests;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.Driver;
+import utils.ReusableMethods;
+
+import java.io.File;
+import java.io.IOException;
 
 public class AllCurrencyTest {
 
@@ -18,7 +26,41 @@ public class AllCurrencyTest {
     AndroidDriver<AndroidElement> driver= Driver.getAndroidDriver();
 
     @Test
-    public void currencyTest(){
+    public void currencyTest() throws IOException {
+        // all currency uygulamasinin yuklendigi dogulanir
+        Assert.assertTrue(driver.isAppInstalled("com.smartwho.SmartAllCurrencyConverter"),"uygulama yukleme basarisiz");
 
+        // uygulamanin acildigi dogrulanir
+        AndroidElement anaSayfaApp=driver.findElementByXPath("//*[@text='CURRENCY CONVERTER']");
+        String uygulamaDogrulamaActual=anaSayfaApp.getText();
+        String expected="CURRENCY CONVERTER";
+        Assert.assertEquals(uygulamaDogrulamaActual,expected,"uyguluma basarili bir sekilde baslatilamadi");
+        AndroidElement ilkKategori=  driver.findElementById("com.smartwho.SmartAllCurrencyConverter:id/linearLayoutPopupHistory");
+        ilkKategori.click();
+
+        // cevirmek istedigimiz para birimi zloty olarak secilir
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"PLN\"))");
+        ReusableMethods.scrollWithUiScrollable("PLN");
+
+        // cevirelecek tutari tuslayalim
+        AndroidElement kategori2 = driver.findElementById("com.smartwho.SmartAllCurrencyConverter:id/SpinnerCurrencyB");
+        kategori2.click();
+        ReusableMethods.scrollWithUiScrollable("TRY");
+
+        driver.findElement(By.id("com.smartwho.SmartAllCurrencyConverter:id/b1")).click();
+        driver.findElement(By.id("com.smartwho.SmartAllCurrencyConverter:id/b5")).click();
+        driver.findElement(By.id("com.smartwho.SmartAllCurrencyConverter:id/b00")).click();
+
+        File ekranfotografi = driver.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(ekranfotografi,new File("mhmt.jpg"));
+
+        ReusableMethods.getScreenshot("t108");
+
+        AndroidElement cevirilenBirim = driver.findElementById("com.smartwho.SmartAllCurrencyConverter:id/EditTextCurrencyB");
+        String sonucParaDegeriPLN = cevirilenBirim.getText();
+
+        // bu islem dolar tl, sweden kron-tl, Japon yeni- tl olarak tekrarlanir ve kullaniciya sms olarak bildirilir
+        ilkKategori.click();
+        ReusableMethods.scrollWithUiScrollable("USD");
     }
 }
